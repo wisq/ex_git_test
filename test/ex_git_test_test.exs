3e10118b537c41f3952cbdba5ef65cb92a0fd93e
Adrian Irving-Beer <wisq@wisq.net>
2023-04-16 19:52:42 -0400

    Initial commit.

--- test/ex_git_test_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ExGitTestTest do
-  use ExUnit.Case
-  doctest ExGitTest
-
-  test "greets the world" do
-    assert ExGitTest.hello() == :world
-  end
-end
---
+++ test/git_test_test.exs
@@ -0,0 +1,8 @@
+defmodule ExGitTest.Test do
+  use ExUnit.Case, async: true
+
+  test "dummy test" do
+    # Just a dummy test so that git.test can test itself.
+    assert true
+  end
+end
