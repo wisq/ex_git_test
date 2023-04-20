628f11652b178ba33a255bb8cdd39c34b15f742f
Adrian Irving-Beer <wisq@wisq.net>
2023-04-20 17:58:19 -0400

    Fix environment leakage between tests.

--- test/git_test_test.exs
+++ test/git_test_test.exs
@@ -3,31 +3,47 @@ defmodule ExGitTest.Test do
 
   import ExUnit.CaptureIO
 
-  @env_var "RUNNING_GIT_TEST_TEST"
-
   test "runs tests in test env, even if MIX_ENV is set" do
-    case System.get_env(@env_var) do
-      "1" ->
+    case git_test(MIX_ENV: :dev) do
+      :inside ->
         assert Mix.env() == :test
 
-      nil ->
-        System.put_env(@env_var, "1")
-        System.put_env("MIX_ENV", "dev")
-        capture_io(fn -> Mix.Tasks.Git.Test.run([]) end)
+      {:outside, _output} ->
+        :ok
     end
   end
 
   test "captures both stdout and stderr" do
-    case System.get_env(@env_var) do
-      "1" ->
+    case git_test() do
+      :inside ->
         IO.puts("message to stdout")
         IO.puts(:stderr, "message to stderr")
 
-      nil ->
-        System.put_env(@env_var, "1")
-        assert out = capture_io(fn -> Mix.Tasks.Git.Test.run([]) end)
-        assert out =~ "message to stdout"
-        assert out =~ "message to stderr"
+      {:outside, output} ->
+        assert output =~ "message to stdout"
+        assert output =~ "message to stderr"
+    end
+  end
+
+  @env_var "RUNNING_GIT_TEST_TESTS"
+  @env_value "yes"
+
+  defp git_test(env \\ []) do
+    case System.get_env(@env_var) do
+      @env_value -> :inside
+      nil -> {:outside, run_git_test(env)}
+    end
+  end
+
+  defp run_git_test(env) do
+    try do
+      System.put_env(@env_var, @env_value)
+      Enum.each(env, fn {k, v} -> System.put_env("#{k}", "#{v}") end)
+
+      capture_io(fn -> Mix.Tasks.Git.Test.run([]) end)
+    after
+      System.delete_env(@env_var)
+      Enum.each(env, fn {k, _} -> System.delete_env("#{k}") end)
     end
   end
 end
